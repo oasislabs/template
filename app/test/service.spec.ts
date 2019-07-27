ee958def635b315ccd42c61d9f724112b1db15d9
Nick Hynes <nhynes@oasislabs.com>
2019-07-27 18:28:38 +0000

    Update tests

--- app/test/service.spec.ts
+++ app/test/service.spec.ts
@@ -1,9 +1,11 @@
-const oasis = require('@oasislabs/client');
+import oasis from '@oasislabs/client';
+
+jest.setTimeout(20000);
 
 describe('Quickstart Test', () => {
   it('should deploy non-confidentially', async () => {
-    const service = await oasis.project.QuickstartClient.deploy({
-      confidential: false,
+    const service = await oasis.workspace.Quickstart.deploy({
+      header: {confidential: false},
     });
 
     expect(service).toBeTruthy();
