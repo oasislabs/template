b93bcfa2ea0318563da9251e18c139dd7a2ce329
Nick Hynes <nhynes@oasislabs.com>
2019-08-08 21:31:08 +0000

    Add deploy script

---
+++ app/scripts/deploy_service.js
@@ -0,0 +1,18 @@
+const chalk = require('chalk');
+const oasis = require('@oasislabs/client');
+
+oasis.workspace.Quickstart.deploy({
+  header: {confidential: false},
+})
+  .then(res => {
+    let addrHex = Buffer.from(res._inner.address).toString('hex');
+    console.log(`    ${chalk.green('Deployed')} Quickstart at 0x${addrHex}`);
+  })
+  .catch(err => {
+    console.error(
+      `${chalk.red('error')}: could not deploy Quickstart: ${err.message}`,
+    );
+  })
+  .finally(() => {
+    oasis.disconnect();
+  });
