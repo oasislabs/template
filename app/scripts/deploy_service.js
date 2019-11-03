7f52a480c8cdcf7af3a490b56b9a334f78be0674
Tony Jin <nijynot@gmail.com>
2019-11-03 01:21:05 +0100

    Fix address hexification in deploy script (#8)

--- app/scripts/deploy_service.js
+++ app/scripts/deploy_service.js
@@ -5,8 +5,7 @@ oasis.workspace.Quickstart.deploy({
   header: {confidential: false},
 })
   .then(res => {
-    let addrHex = Buffer.from(res.address).toString('hex');
-    console.log(`    ${chalk.green('Deployed')} Quickstart at 0x${addrHex}`);
+    console.log(`    ${chalk.green('Deployed')} Quickstart at ${res.address.hex}`);
   })
   .catch(err => {
     console.error(chalk.red('error'), err);
